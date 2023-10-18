92ae08623e79ce75fe152cf13b442ac54675c283
nilswen <nilswentingpro@gmail.com>
2023-10-18 03:37:51 +0200

    add sqlite db, add Server / Clients interraction

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -2,18 +2,106 @@ package org.example;
 import java.net.*;
 import java.util.Hashtable;
 import java.io.*;
+import java.net.InetAddress;
+import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    private  String username;
+
+    private  String password;
+
+
+
+
+
+    public Client(Socket socket, String username, String password){
+       try {
+           this.socket = socket;
+           this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+           this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+           this.username = username;
+           this.password = password;
+       }catch (IOException err){
+           closeEverything(socket, bufferedReader, bufferedWriter);
+       }
+   }
+
+   public void sendMessage(){
         try {
-            Socket s = new Socket("localhost", 6500);
-            InputStream in = s.getInputStream();
-            ObjectInputStream ois = new ObjectInputStream(in);
-            final Hashtable<String, String> message =  (Hashtable<String, String>) ois.readObject();
-            System.out.println(message.get("test"));
-        } catch (IOException | ClassNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            bufferedWriter.write((username));
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            Scanner scanner = new Scanner(System.in);
+            while(socket.isConnected()){
+                String messageToSend = scanner.nextLine();
+                bufferedWriter.write(username + ": " + messageToSend);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+        }catch (IOException err){
+            closeEverything(socket, bufferedReader, bufferedWriter);
         }
-    }
-}
+   }
+
+
+   public void listenForMessage(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String messageFromGroupChat;
+                while (socket.isConnected()){
+                    try {
+                        messageFromGroupChat = bufferedReader.readLine();
+                        System.out.println(messageFromGroupChat);
+                    }catch (IOException err){
+                        closeEverything(socket, bufferedReader, bufferedWriter);
+                    }
+                }
+            }
+        }).start();
+   }
+
+   public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+       try {
+           if(bufferedReader != null){
+               bufferedReader.close();
+           }
+
+           if (bufferedWriter != null){
+               bufferedWriter.close();
+           }
+
+           if (socket != null){
+               socket.close();
+           }
+       }catch (IOException err){
+           err.printStackTrace();
+       }
+   }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entrez votre pseudo : ");
+        String username = scanner.nextLine();
+        System.out.println("Entre votre mot de passe: ");
+        String password = scanner.nextLine();
+        Socket socket = new Socket("localhost", 6000);
+        Client client = new Client(socket, username, password);
+
+        // Démarrer un thread pour écouter les messages du serveur
+        Thread listenThread = new Thread(client::listenForMessage);
+        listenThread.start();
+
+        // Démarrer un thread pour envoyer des messages
+        Thread sendThread = new Thread(client::sendMessage);
+        sendThread.start();    }
+
+   }
+
+
+
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,46 +1,82 @@
 package org.example;
 import java.net.*;
+import java.sql.*;
 import java.util.Hashtable;
 import java.io.*;
+import java.net.InetAddress;
 
 public class Server {
-    public static void main(String[] args){
 
-        try {
-            ServerSocket ss =  new ServerSocket(6500);
-            Hashtable <String, String> message = new Hashtable<String, String>();
-            message.put("test", "bonjour ceci est un test tes grand morts");
- // implémenter authentification ici je pense 
-            Socket client = ss.accept();
-            System.out.println(message.get("test")); 
-           
-            System.out.println("nouvelle connection");
+        private ServerSocket ss;
 
-            OutputStream out = client.getOutputStream();
-            ObjectOutputStream oos = new ObjectOutputStream(out);
-            oos.writeObject(message);
-            client.close();            
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+
+        public Server(ServerSocket ss){
+            this.ss = ss;
         }
 
+    public void startServer(){
 
+            try {
+                InetAddress ip = InetAddress.getLocalHost();
+                String hostAddress = ip.getHostAddress();
+                System.out.println("Adresse ip du serveur : " + hostAddress);
 
+                while(!ss.isClosed()){
+                    Socket socket = ss.accept();
+                    System.out.println("Nouvelle connection !" );
+                    ClientHandler clientHandler = new ClientHandler(socket);
 
+                    Thread thread = new Thread(clientHandler);
+                    thread.start();
 
-
-
-
-
-
-
-
-
-
-
-        //CreateDbHandler db = new CreateDbHandler();
-        //db.getConn();
-        
+                }
+            }catch (IOException err){
+                err.printStackTrace();
+            }
     }
+
+
+
+    public void initDb(){
+            try(Connection connection = DriverManager.getConnection("jdbc:sqlite:galactic_messenger.db");
+                Statement statement = connection.createStatement()) {
+                String createTableSQL = "CREATE TABLE IF NOT EXISTS clients (" +
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        "username TEXT NOT NULL," +
+                        "password TEXT NOT NULL)";
+                statement.executeUpdate(createTableSQL);
+                System.out.println("table crée avec succées");
+
+            }catch (SQLException err){
+                err.printStackTrace();
+            }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(6000);
+        Server server = new Server(ss);
+        server.initDb();
+        server.startServer();
+
+    }
+
+
+
+
+
+
+
+
+   /*public void shutServer(){
+            try {
+                if (ss != null){
+                    ss.close();
+                }
+            }catch (IOException err){
+                err.printStackTrace();
+            }
+    }*/
+        
+
 }
